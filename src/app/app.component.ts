6b037a77a532476162782345989b2e8eba1e3643
marciliodevjava <marciliodevjava@gmail.com>
2022-05-12 16:22:41 -0300

    ByteBank com Angular

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'bytebank';
+  transferencias: any[] = [];
+
+
+  transferir($event: any){
+    console.log($event);
+    const transferencia = {...$event, data: new Date()};
+    this.transferencias.push(transferencia);
+
+  }
+}
